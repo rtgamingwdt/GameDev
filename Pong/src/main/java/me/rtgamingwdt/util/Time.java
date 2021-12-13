3bc45506f0569be076473ef9f1e9e7a175b0b7b5
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-12-13 06:37:42 +0000

    Decided to work on flappybird instead of pong

--- Pong/src/main/java/me/rtgamingwdt/util/Time.java
+++
@@ -1,9 +0,0 @@
-package me.rtgamingwdt.util;
-
-public class Time {
-  public static double timeStarted = System.nanoTime();
-
-  public static double getTime() {
-    return (System.nanoTime() - timeStarted) * 1E-9;
-  }
-}
